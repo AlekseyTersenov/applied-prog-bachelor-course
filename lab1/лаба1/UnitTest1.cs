9c137575a8a25c7a99c47b12acacdce135308747
AlekseyTersenov <feberol@bk.ru>
2017-05-29 11:37:31 +0300

    Update UnitTest1.cs

--- lab1/лаба1/UnitTest1.cs
+++ lab1/лаба1/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PointOnСrossing;
 
@@ -33,5 +33,21 @@ namespace UnitTestProject1
 
             Assert.AreEqual(Line.getCrossing(l1, l2), new Point(1, 1));
         } 
+		[TestMethod] 
+        public void MethodForLineEquals() 
+        { 
+            Line l1 = new Line(new Point(1, 1), new Point(2, 2)); 
+            Line l2 = new Line(new Point(1, 2), new Point(2, 3)); 
+
+            if ((Math.Abs(l1.one.x - l2.one.x) == (Math.Abs(l1.two.x - lw.two.x))) && (Math.Abs(l1.one.y - l2.one.y) == (Math.Abs(l1.two.y - lw.two.y)))) 
+            { 
+                bool t = true; 
+            } 
+            else 
+            { 
+                bool t = false; 
+            } 
+            Assert.AreEqual(t, true); 
+        }
     }
 }
